cf0b729a07c9f087e83c663024c068704238ac12
Samarth Vyomeshchandra Raval <sm978741@dal.ca>
2022-04-15 18:29:04 -0400

    Multiplication

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Samarth Raval");
+        int a = 5 + 10;
+        System.out.println("Sum would be = " + a);
+        int mul = 5 * 10;
+        System.out.println("Multiplication = " + mul);
     }
 }
