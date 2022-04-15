cae8b36c4d34f778bf232162a1f2dd9ee619b41d
Samarth Vyomeshchandra Raval <sm978741@dal.ca>
2022-04-15 18:48:24 -0400

    new class added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,5 +6,20 @@ public class HelloWorld {
         System.out.println("Sum would be = " + a);
         int mul = 5 * 10;
         System.out.println("Multiplication = " + mul);
+
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printStatement();
+        System.out.println("return value = "+ helloWorld.subtraction());
+    }
+
+    public void printStatement()
+    {
+        System.out.println("Just for checking how method is working");
+    }
+
+    public int subtraction()
+    {
+        System.out.println("Subtraction = "+ (10-2));
+        return 9;
     }
 }
